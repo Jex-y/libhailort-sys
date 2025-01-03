18a31d20c8e65fe34093fa4d1fa64d541c07c024
Edward Jex <edwardjex@live.co.uk>
2025-01-03 18:37:26 +0000

    modified build.rs

--- build.rs
+++ build.rs
@@ -3,14 +3,12 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let dst = cmake::Config::new("hailort")
-        .cflag("-Wno-dev")
-        // .build_target("libhailort")
-        .build();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let dst = cmake::Config::new("hailort").build();
 
     let obj_path = dst.join("build/hailort/libhailort/src/libhailort.so");
-    // let lib_path = dst.join("build/hailort/libhailort/src/libhailort.a");
-    let lib_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("libhailort.a");
+    let lib_path = out_dir.clone().join("libhailort.a");
 
     let headers_path = dst.join("include/hailo/hailort.h");
     let headers_path_str = headers_path
@@ -18,11 +16,7 @@ fn main() {
         .expect("Header path is not a valid string");
 
     // Link to the hailort library
-    println!(
-        "cargo:rustc-link-search={}",
-        // obj_path.parent().unwrap().display()
-        env::var("OUT_DIR").unwrap()
-    );
+    println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=hailort");
 
     // Generate .a file
@@ -43,8 +37,7 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
     bindings
-        .write_to_file(out_path)
+        .write_to_file(out_dir.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
